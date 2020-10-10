e825be0ce2289999a6d2fa2725f03ea7b79ee03f
prudiusDenys <prdslife@gmail.com>
2020-10-10 16:17:50 +0200

    Updates

---
+++ precache-manifest.e788dc6ba6d52f3ffe56386f9596b0ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9605f16b6a416c5a4c085855074d3896",
+    "url": "/prudiusDenys/profile/prudiusDenys/index.html"
+  },
+  {
+    "revision": "e5f8f5e1abecc2023ce1",
+    "url": "/prudiusDenys/profile/prudiusDenys/static/css/main.2489b53d.chunk.css"
+  },
+  {
+    "revision": "a03b098fa2e405dc83d0",
+    "url": "/prudiusDenys/profile/prudiusDenys/static/js/2.ee76e245.chunk.js"
+  },
+  {
+    "revision": "1937a0fc6634269e5f47f8181e27fdc7",
+    "url": "/prudiusDenys/profile/prudiusDenys/static/js/2.ee76e245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e5f8f5e1abecc2023ce1",
+    "url": "/prudiusDenys/profile/prudiusDenys/static/js/main.d811bff7.chunk.js"
+  },
+  {
+    "revision": "3cfad0ef3e2c18dac432",
+    "url": "/prudiusDenys/profile/prudiusDenys/static/js/runtime-main.8db65a9c.js"
+  },
+  {
+    "revision": "84fd14e6320a1044440badc34daa0833",
+    "url": "/prudiusDenys/profile/prudiusDenys/static/media/profileBC.84fd14e6.jpg"
+  }
+]);
